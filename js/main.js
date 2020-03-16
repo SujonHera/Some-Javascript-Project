dc01a977f452f988c153bd3a87e74f53541c78d2
sujonhossainbd <sujon.hossain758@gmail.com>
2020-03-16 17:36:11 +0600

    drum kit javascript simple project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+var typed = new Typed('.element', {
+    strings: [
+        "I'm Front-End Web Developer", "Some Javascript Project Here",
+        "1. LoveChecker Calculator", "2. Weather Could", "3. Drum Kit With Keyboard Keys",
+        "4. Live Analog Clock", "5. Propose Your Crush"
+    ],
+    loop:true,
+    typeSpeed: 25,
+    startDelay: 100,
+    backSpeed: 25,
+    smartBackspace: true,
+    backDelay: 900,
+
+});
